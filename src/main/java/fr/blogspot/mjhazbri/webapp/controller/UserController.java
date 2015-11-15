6e99b0eba31172b20e62edbe50a8f78883912568
Jalel <jalel.hazbri@gmail.com>
2015-11-15 20:05:19 +0100

    Spring Mvc

--- src/main/java/fr/blogspot/mjhazbri/webapp/controller/UserController.java
+++ src/main/java/fr/blogspot/mjhazbri/webapp/controller/UserController.java
@@ -3,10 +3,19 @@
  */
 package fr.blogspot.mjhazbri.webapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author Jalel
  *
  */
+
+@Controller
 public interface UserController {
 
+	@RequestMapping("/hello")
+	ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name);
 }
--- src/main/java/fr/blogspot/mjhazbri/webapp/controller/impl/UserControllerImpl.java
+++ src/main/java/fr/blogspot/mjhazbri/webapp/controller/impl/UserControllerImpl.java
@@ -3,6 +3,9 @@
  */
 package fr.blogspot.mjhazbri.webapp.controller.impl;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import fr.blogspot.mjhazbri.webapp.controller.UserController;
 
 /**
@@ -10,5 +13,15 @@ import fr.blogspot.mjhazbri.webapp.controller.UserController;
  *
  */
 public class UserControllerImpl implements UserController {
+	String message = "Welcome to Spring MVC!";
 
+	@Override
+	public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
 }
