3a6a34c3a7eb551ef56f27408a64dda111176f2f
Jalel <jalel.hazbri@gmail.com>
2015-11-16 00:00:29 +0100

    Spring MVC rest.

--- src/main/java/fr/blogspot/mjhazbri/webapp/controller/UserController.java
+++ src/main/java/fr/blogspot/mjhazbri/webapp/controller/UserController.java
@@ -3,19 +3,18 @@
  */
 package fr.blogspot.mjhazbri.webapp.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import fr.blogspot.mjhazbri.webapp.data.entities.User;
 
 /**
  * @author Jalel
  *
  */
 
-@Controller
 public interface UserController {
 
-	@RequestMapping("/hello")
-	ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name);
+	ModelAndView showMessage(String name);
+
+	User toJson();
 }
--- src/main/java/fr/blogspot/mjhazbri/webapp/controller/impl/UserControllerImpl.java
+++ src/main/java/fr/blogspot/mjhazbri/webapp/controller/impl/UserControllerImpl.java
@@ -3,25 +3,49 @@
  */
 package fr.blogspot.mjhazbri.webapp.controller.impl;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.blogspot.mjhazbri.webapp.controller.UserController;
+import fr.blogspot.mjhazbri.webapp.data.entities.User;
 
 /**
  * @author Jalel
  *
  */
+
+@Controller
 public class UserControllerImpl implements UserController {
 	String message = "Welcome to Spring MVC!";
 
-	@Override
-	public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
+	private static final Logger logger = Logger.getLogger(UserControllerImpl.class);
 
-		ModelAndView mv = new ModelAndView("helloworld");
+	@Override
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		logger.debug("in controller  .... !");
+		ModelAndView mv = new ModelAndView("user");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
 		return mv;
+	}
+
+	@Override
+	@RequestMapping(value = "/toJson", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody User toJson() {
+		
+		logger.debug("toJson in controller  .... !");
+		User user = new User();
+		user.setId("0123456789");
+		user.setLasteName("lastName");
+		user.setFirstName("firstName");
+		user.setEmail("test@testcom");
+		return user;
 	}
 }
